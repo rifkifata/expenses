012bb90627baaf6759fa0a5181b8625c6840f62e
teuku rifki dhulul fata <rifkifata@gmail.com>
2022-10-23 23:57:19 +0700

    make new function new row

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,58 +1,48 @@
 <template>
   <div class="window-height">
     <q-table
-          :title="title"
-          :rows="posts"
-          :columns="columns"
-          row-key="id"
-          dark
-          color="amber"
-          class="col"
-        />
-
-    <q-table
-      title="Treats"
+      :title="title"
       :rows="posts"
       :columns="columns"
       row-key="id"
+      dark
       binary-state-sort
     >
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="name" :props="props">
             {{ props.row.name }}
-            <q-popup-edit v-model="props.row.name" v-slot="scope">
-              <q-input v-model="scope.value" dense autofocus counter />
+            <q-popup-edit v-model="props.row.name" title="Update name" buttons v-slot="scope">
+              <q-input type="textarea" v-model="scope.value" dense autofocus/>
             </q-popup-edit>
           </q-td>
-          <q-td key="calories" :props="props">
-            {{ props.row.calories }}
-            <q-popup-edit v-model="props.row.calories" title="Update calories" buttons v-slot="scope">
-              <q-input type="number" v-model="scope.value" dense autofocus />
-            </q-popup-edit>
-          </q-td>
-          <q-td key="fat" :props="props">
-            <div class="text-pre-wrap">{{ props.row.fat }}</div>
-            <q-popup-edit v-model="props.row.fat" v-slot="scope">
+          <q-td key="category" :props="props">
+            {{ props.row.category }}
+            <q-popup-edit v-model="props.row.category" title="Update category" buttons v-slot="scope">
               <q-input type="textarea" v-model="scope.value" dense autofocus />
             </q-popup-edit>
           </q-td>
-          <q-td key="carbs" :props="props">
-            {{ props.row.carbs }}
-            <q-popup-edit v-model="props.row.carbs" title="Update carbs" buttons persistent v-slot="scope">
+          <q-td key="type" :props="props">
+            <div class="text-pre-wrap">{{ props.row.type }}</div>
+            <q-popup-edit v-model="props.row.type" title="Update type" buttons v-slot="scope">
+               <q-input type="textarea" v-model="scope.value" dense autofocus />
+            </q-popup-edit>
+          </q-td>
+          <q-td key="price" :props="props">
+            {{ props.row.price }}
+            <q-popup-edit v-model="props.row.price" title="Update price" buttons persistent v-slot="scope">
               <q-input type="number" v-model="scope.value" dense autofocus hint="Use buttons to close" />
             </q-popup-edit>
           </q-td>
-          <q-td key="protein" :props="props">{{ props.row.protein }}</q-td>
-          <q-td key="sodium" :props="props">{{ props.row.sodium }}</q-td>
-          <q-td key="calcium" :props="props">{{ props.row.calcium }}</q-td>
-          <q-td key="iron" :props="props">{{ props.row.iron }}</q-td>
         </q-tr>
       </template>
     </q-table>
 
-        <q-btn @click.stop="prevPosts()" color="red" icon="mail" icon-right="send" label="Left"/>
-        <q-btn @click.stop="nextPosts()" color="red" icon="mail" icon-right="send" label="Right"/>
+        <q-btn @click.stop="nextPrevPosts(-1)" color="red" icon="mail" icon-right="send" label="Left"/>
+        <q-btn @click.stop="nextPrevPosts(1)" color="red" icon="mail" icon-right="send" label="Right"/>
+        <q-btn @click.stop="sendUpdate()" color="red" icon="mail" icon-right="send" label="Send"/>
+        <q-btn @click.stop="newRow()" color="red" icon="mail" icon-right="send" label="New Row"/>
+
         <div id="date" style="height:100px; width:300px;">
         </div>
         <button id="prev">prev</button>
@@ -75,6 +65,9 @@ function getTheDate(getDate) {
             var theCDate = new Date(getDate);
             return days[theCDate.getDay()] + ', ' + theCDate.getDate() + ' ' + months[theCDate.getMonth()] + ' ' + theCDate.getFullYear();
           }
+function hitApi () {
+
+}
 // import { expenses } from '../repository/database'
 export default {
   name: 'PageIndex',
@@ -113,7 +106,8 @@ export default {
       ],
       posts: [],
       date: "",
-      title: ""
+      title: "",
+      puts: ""
       // carousel
       // controlType: ref('outline'),
       // controlTypeOptions: [
@@ -134,7 +128,7 @@ export default {
     this.getPosts()
   },
   mounted () {
-    //this.getDate()
+    this.newRow()
   },
   methods: {
     getPosts () {
@@ -147,46 +141,15 @@ export default {
           this.title = getTheDate(dateToday)
           let arr = res.data.record
           this.posts = arr.filter(({ date }) => date === dateTodayISO)
-          // let date = new Date();
-          // add a day
-          // date.setDate(date.getDate() + -1);
-          // console.log(date.toLocaleDateString('id'))
-          // const data = res.data.record
-
-          // const groups = data.reduce((groups, game) => {
-          //   const date = game.date.split('T')[0]
-          //   if (!groups[date]) {
-          //     groups[date] = []
-          //   }
-          //   groups[date].push(game)
-          //   return groups
-          // }, {})
-
-          // const groupArrays = Object.keys(groups).map((date) => {
-          //   return {
-          //     date,
-          //     games: groups[date]
-          //   }
-          // })
-
-          // console.log(groupArrays)
-          // let arr = res.data.record
-          // console.log(arr[0].date)
-          // const aquaticCreatures =  filtered.filter(function(filtered) {
-          //   return filtered.date == "Ocean";
-          // });
-
-          // const filterByExpiration = (selectedDate) => arr.filter(({ updated_date }) => new Date(updated_date) <= new Date(selectedDate));
-
         })
         .catch((err) => {
           console.log(err)
         })
     },
-    nextPosts () {
+    nextPrevPosts (n) {
       this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
         .then((res) => {
-          dateToday.setDate(dateToday.getDate() + 1)
+          dateToday.setDate(dateToday.getDate() + n)
           dateToday.setHours(0,0,0,0)
           let dateTodayISO = dateToday.toISOString()
 
@@ -201,24 +164,63 @@ export default {
           console.log(err)
         })
     },
-    prevPosts () {
+    sendUpdate () {
       this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
         .then((res) => {
-          dateToday.setDate(dateToday.getDate() - 1)
-          dateToday.setHours(0,0,0,0)
-          let dateTodayISO = dateToday.toISOString()
-
-          this.title = getTheDate(dateToday)
-          console.log(this.title)
-
-          let arr = res.data.record
-          this.posts = arr.filter(({ date }) => date === dateTodayISO)
-
-          console.log(this.posts)})
+          let arrOri = res.data.record
+          let arrEdit = this.posts
+          arrEdit.forEach(element => {
+                      const itemIndex = arrOri.findIndex(o => o.trx_id === element.trx_id)
+                      if(itemIndex > -1) {
+                          arrOri[itemIndex] = element
+                      } else {
+                          arrOri = arrOri.push(element)
+                      }
+                  })
+          this.posts = arrOri
+        })
         .catch((err) => {
           console.log(err)
         })
+        this.$axios.put('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82',
+          this.posts,
+        {
+          headers : {
+          'Content-Type' : 'application/json',
+          'X-Master-Key'  : '$2b$10$qfhZvw7jnDo4msI1o/t8kuKntlVp6/tqS/FqNi659LVbmLOxfsm9u'
+        }})
+          .then((res) => {
+          console.log(res)
+        }).catch((err)=> {
+          console.log(err)
+        })
     },
+    newRow () {
+      let arrOri = this.posts
+
+      const ids = arrOri.map(object => {
+        return object.trx_id;
+      });
+      // const max = Math.max(...ids)
+      let newObject =  {
+        "trx_id": Math.max(...ids),
+        "name": "",
+        "price": "",
+        "date": "2022-10-24T17:00:00.000Z",
+        "time": "13.00",
+        "is_deleted": 0,
+        "created_date": "04-08-1999",
+        "updated_date": "05-08-1999",
+        "category": "",
+        "type": ""
+      }
+      arrOri.push(newObject)
+      this.posts = arrOri
+       console.log(this.posts)
+       console.log(newObject)
+
+      // this.posts = arrOri.push(newObject)
+    }
     // getDate() {
     //   if (!Date.now) {
     //     Date.now = function() {
@@ -277,6 +279,9 @@ export default {
 
     // todo : BIKIN BISA NEXT DI CARAOUSEL
     // todo : ubahdata itu (put) harus get dlu semuanya -> kirim ke function -> ubah data -> returned data ori + udah d ubah
+
+    //HARUSNYA ADA FUNGSI BARU YANG DIJALANKAN UNTUK GET DATA FULL, TRS TAMPUNG KE VARIABLE GLOBAL
+    //VARIABLE INILAH YANG HARUSNYA D OTAK ATIK NTUK EDIT/NEW ROW DLL, BUKANNYA MALAH GET AXIOS BARU.
   }
 }
 </script>
