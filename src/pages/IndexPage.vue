4984ed37c45f27ea2c0426302d2889ece748ba29
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2022-10-24 16:51:47 +0700

    make some new function and cleaning the code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -8,6 +8,9 @@
       dark
       binary-state-sort
     >
+     <template v-slot:top-right>
+        <text-body1>Rp. 24.0000</text-body1>
+      </template>
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="name" :props="props">
@@ -40,17 +43,39 @@
 
         <q-btn @click.stop="nextPrevPosts(-1)" color="red" icon="mail" icon-right="send" label="Left"/>
         <q-btn @click.stop="nextPrevPosts(1)" color="red" icon="mail" icon-right="send" label="Right"/>
-        <q-btn @click.stop="sendUpdate()" color="red" icon="mail" icon-right="send" label="Send"/>
+        <q-btn @click.stop="sendUpdateData()" color="red" icon="mail" icon-right="send" label="Send"/>
         <q-btn @click.stop="newRow()" color="red" icon="mail" icon-right="send" label="New Row"/>
+        <q-btn @click.stop="getPosts()" color="red" icon="mail" icon-right="send" label="New Row"/>
 
         <div id="date" style="height:100px; width:300px;">
         </div>
-        <button id="prev">prev</button>
-            <button id="next">next</button>
           </div>
 </template>
 
 <script lang="ts">
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBRWZ5eeZ1YpgYTgVqEGpzAS6iTa3elDy0",
+//   authDomain: "expenses-bae47.firebaseapp.com",
+//   projectId: "expenses-bae47",
+//   storageBucket: "expenses-bae47.appspot.com",
+//   messagingSenderId: "585378571530",
+//   appId: "1:585378571530:web:ad17176556cd715e00c6f3",
+//   measurementId: "G-RCTN3S42W8"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
 // carousel
 import { ref } from 'vue'
 let dateToday = new Date ()
@@ -65,9 +90,6 @@ function getTheDate(getDate) {
             var theCDate = new Date(getDate);
             return days[theCDate.getDay()] + ', ' + theCDate.getDate() + ' ' + months[theCDate.getMonth()] + ' ' + theCDate.getFullYear();
           }
-function hitApi () {
-
-}
 // import { expenses } from '../repository/database'
 export default {
   name: 'PageIndex',
@@ -107,7 +129,8 @@ export default {
       posts: [],
       date: "",
       title: "",
-      puts: ""
+      puts: "",
+      data: []
       // carousel
       // controlType: ref('outline'),
       // controlTypeOptions: [
@@ -125,65 +148,26 @@ export default {
     }
   },
   beforeMount () {
-    this.getPosts()
+    this.getAllData()
   },
   mounted () {
-    this.newRow()
+    this.getPosts()
   },
+  created () {},
   methods: {
-    getPosts () {
-      this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
+    //function connecting axios to API bin
+    async getAllData () {
+      await this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
         .then((res) => {
-          // let theDate = formatDate(new Date(new Date))
-          dateToday.setHours(0,0,0,0)
-          let dateTodayISO = dateToday.toISOString()
-
-          this.title = getTheDate(dateToday)
-          let arr = res.data.record
-          this.posts = arr.filter(({ date }) => date === dateTodayISO)
-        })
+          this.data = res.data.record
+          })
         .catch((err) => {
           console.log(err)
         })
     },
-    nextPrevPosts (n) {
-      this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
-        .then((res) => {
-          dateToday.setDate(dateToday.getDate() + n)
-          dateToday.setHours(0,0,0,0)
-          let dateTodayISO = dateToday.toISOString()
-
-          this.title = getTheDate(dateToday)
-          console.log(this.title)
-
-          let arr = res.data.record
-          this.posts = arr.filter(({ date }) => date === dateTodayISO)
-
-          console.log(this.posts)})
-        .catch((err) => {
-          console.log(err)
-        })
-    },
-    sendUpdate () {
-      this.$axios.get('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82')
-        .then((res) => {
-          let arrOri = res.data.record
-          let arrEdit = this.posts
-          arrEdit.forEach(element => {
-                      const itemIndex = arrOri.findIndex(o => o.trx_id === element.trx_id)
-                      if(itemIndex > -1) {
-                          arrOri[itemIndex] = element
-                      } else {
-                          arrOri = arrOri.push(element)
-                      }
-                  })
-          this.posts = arrOri
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-        this.$axios.put('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82',
-          this.posts,
+    async updateData (arr) {
+      await this.$axios.put('https://api.jsonbin.io/v3/b/6347ed932b3499323bdd1e82',
+          arr,
         {
           headers : {
           'Content-Type' : 'application/json',
@@ -195,32 +179,82 @@ export default {
           console.log(err)
         })
     },
-    newRow () {
-      let arrOri = this.posts
 
+    //utils Function
+    async getPosts () {
+      dateToday.setHours(0,0,0,0)
+      let dateTodayISO = dateToday.toISOString()
+      this.title = getTheDate(dateToday)
+      await this.getAllData()
+      let arr = this.data
+      this.posts = arr.filter(({ date }) => date === dateTodayISO)
+    },
+    nextPrevPosts (n) {
+        dateToday.setDate(dateToday.getDate() + n)
+        dateToday.setHours(0,0,0,0)
+        let dateTodayISO = dateToday.toISOString()
+        this.title = getTheDate(dateToday)
+        let arr = this.data
+        this.posts = arr.filter(({ date }) => date === dateTodayISO)
+    },
+    async sendUpdateData () {
+        let arrOri = this.data
+        let arrEdit = this.posts
+        arrEdit.forEach(element => {
+                    const itemIndex = arrOri.findIndex(o => o.trx_id === element.trx_id)
+                    if(itemIndex > -1) {
+                        arrOri[itemIndex] = element
+                    } else {
+                        arrOri = arrOri.push(element)
+                    }
+                })
+        this.data = arrOri
+        await this.updateData(this.data)
+    },
+    async newRow () {
+      let arrOri = await this.data
+      let arrEdit = await this.posts
+      //format Date
+      let dateTodayISO = dateToday.toISOString()
+      const yyyy = dateToday.getFullYear()
+      let mm = dateToday.getMonth() + 1
+      let dd = dateToday.getDate()
+      if (dd < 10) dd = '0' + dd
+      if (mm < 10) mm = '0' + mm
+      const formattedToday = dd + '-' + mm + '-' + yyyy
+
+      //grouping the trx_id
       const ids = arrOri.map(object => {
-        return object.trx_id;
-      });
-      // const max = Math.max(...ids)
+        return object.trx_id
+      })
+
       let newObject =  {
-        "trx_id": Math.max(...ids),
+        "trx_id": Math.max(...ids) + 1,
         "name": "",
         "price": "",
-        "date": "2022-10-24T17:00:00.000Z",
-        "time": "13.00",
+        "date": dateTodayISO,
         "is_deleted": 0,
-        "created_date": "04-08-1999",
-        "updated_date": "05-08-1999",
-        "category": "",
-        "type": ""
+        "created_date": formattedToday,
+        "category": "Primary",
+        "type": "Debit"
       }
       arrOri.push(newObject)
-      this.posts = arrOri
-       console.log(this.posts)
-       console.log(newObject)
+      arrEdit.push(newObject)
 
-      // this.posts = arrOri.push(newObject)
+      this.data = arrOri
+      this.posts = arrEdit
+    },
+    formatDate () {
+
+    },
+    total() {
+
+    },
+    sumToday() {
+
     }
+
+
     // getDate() {
     //   if (!Date.now) {
     //     Date.now = function() {
@@ -282,6 +316,7 @@ export default {
 
     //HARUSNYA ADA FUNGSI BARU YANG DIJALANKAN UNTUK GET DATA FULL, TRS TAMPUNG KE VARIABLE GLOBAL
     //VARIABLE INILAH YANG HARUSNYA D OTAK ATIK NTUK EDIT/NEW ROW DLL, BUKANNYA MALAH GET AXIOS BARU.
+    //TODO: Updated Date!
   }
 }
 </script>
