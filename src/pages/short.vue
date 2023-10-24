98f53cadf180927df36a0b8d5f6110be2a5c94e4
teuku rifki dhulul fata <rifkifata@gmail.com>
2023-10-24 21:39:52 +0700

    fix animation

--- src/pages/short.vue
+++ src/pages/short.vue
@@ -2,11 +2,11 @@
   <div class="q-pa-md example-row-all-breakpoints">
     <!-- <div class="q-gutter-y-md column" style="max-width: 1000px"> -->
     <div class="row items-center">
-      <div class="col-2 q-mr-xs" align="right" >https:// </div>
+      <div class="col-2 q-mr-xs" align="right">https:// </div>
       <div class="col-10">
-        <q-input outlined v-model="text" placeholder="www.youtube.com" :dense="dense">
+        <q-input class="my-element" outlined v-model="sourcePath" for="txtSourcePath" placeholder="www.verylongendpoint.com" :dense="dense">
           <template v-slot:after>
-            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+            <q-icon name="close" @click="sourcePath = ''" class="cursor-pointer" />
           </template>
         </q-input>
       </div>
@@ -14,27 +14,38 @@
     <div class="row items-center">
       <div class="col-2" align="right">teuku.my.id/</div>
       <div class="col-10">
-        <q-input outlined v-model="text" placeholder="ikyganteng" :dense="dense">
+        <q-input outlined v-model="shortedPath" for="txtShortedPath" placeholder="short" :dense="dense">
           <template v-slot:after>
-            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+            <q-icon name="close" @click="shortedPath = ''" class="cursor-pointer" />
+          </template>
+        </q-input>
+      </div>
+    </div>
+    <div class="row items-center">
+      <div class="col-2" align="right">Submit By : </div>
+      <div class="col-10">
+        <q-input outlined v-model="author" for="txtAuthor" placeholder="ikyganteng" :dense="dense">
+          <template v-slot:after>
+            <q-icon name="close" @click="author = ''" class="cursor-pointer" />
           </template>
         </q-input>
       </div>
     </div>
     <div class="row items-center">
       <div class="col-2" align="right">
-          <!-- <q-btn round color="purple" icon="navigation" class="square" id="btnPost" /> -->
-          <!-- <img class="tombol" src="https://cdn.quasar.dev/logo-v2/svg/logo-dark.svg" alt="Quasar Logo" width="48" height="48"> -->
-      <q-btn class="rotate-90" round color="purple" icon="navigation" onclick="this.style['WebkitTransform'] = 'rotate(' + 180 +'deg)';"/>
+        <!--        <q-btn class="rotate-90" round color="black" icon="navigation" onclick="this.style['WebkitTransform'] = 'rotate(' + 180 +'deg)';validate()" /> -->
+        <q-btn class="rotate-90" round color="black" icon="navigation" @click="validate()" />
 
       </div>
-<!-- hint="Made with Pride by Teuku Rifki Dhulul Fata" -->
-<div class="col-10">
-        <q-input outlined v-model="text"  :dense="dense" readonly>
-        <template v-slot:after>
-            <q-icon name="content_copy" @click="text = ''" class="cursor-pointer" />
+      <!-- hint="Made with Pride by Teuku Rifki Dhulul Fata" -->
+      <div class="col-10">
+        <q-input outlined v-model="result" :dense="dense" readonly>
+          <template v-slot:after>
+            <q-icon id="txtResult" name="content_copy" @click="copyToClipBoard()" class="cursor-pointer" />
           </template>
-          <template v-slot:hint> Destination </template>
+          <template v-slot:hint>
+            Destination
+          </template>
         </q-input>
       </div>
     </div>
@@ -48,11 +59,11 @@
 
 
 </template>
-<style lang="sass">
-.example-row-all-breakpoints
-  .row > div
-  .row + .row
-    margin-top: 1rem
+<style>
+  .example-row-all-breakpoints {
+    margin-top: 1rem;
+  }
+  
 </style>
 
 <script lang="ts">
@@ -91,15 +102,70 @@
     },
     data() {
       return {
-        text: '',
+        result: '',
         ph: '',
         dense: true,
-        show: false
+        show: false,
+
+        sourcePath: "",
+        shortedPath: "",
+        author: null
       }
     },
     beforeMount() {},
     mounted() {},
     created() {},
-    methods: {}
+    methods: {
+      async copyToClipBoard() {
+        await navigator.clipboard.writeText(this.result);
+      },
+      async post() {
+        await this.$axios.post('https://teuku.cyclic.app/short/', {
+          "sourcePath": this.sourcePath,
+          "shortedPath": this.shortedPath,
+          "author": this.author
+        },{
+          headers: {}
+        })
+          .then((res) => {
+            this.result = "teuku.my.id/" + res.data.props.shortedPath
+          }).catch((err) => {
+            console.log(err.response.data)
+            this.result = err.response.data.message
+          })
+      },
+      validate() {
+        var sourcePath = this.sourcePath
+        var shortedPath = this.shortedPath
+        var author = this.author
+        var element = document.getElementById('txtSourcePath');
+        if (sourcePath == "") {
+          animateCSS('#txtSourcePath', 'shakeX');
+          console.log("sourcePath Cannot Empty")
+        }
+        if (shortedPath == "") {
+          animateCSS('#txtShortedPath', 'shakeX');
+          console.log("shortedPath Cannot Empty")
+        }
+        return true
+      }
+    }
   }
+  const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+      const animationName = `${prefix}${animation}`;
+      const node = document.querySelector(element);
+
+      node.classList.add(`${prefix}animated`, animationName);
+
+      // When the animation ends, we clean the classes and resolve the Promise
+      function handleAnimationEnd(event) {
+        event.stopPropagation();
+        node.classList.remove(`${prefix}animated`, animationName);
+        resolve('Animation ended');
+      }
+
+      node.addEventListener('animationend', handleAnimationEnd, { once: true });
+    });
 </script>
