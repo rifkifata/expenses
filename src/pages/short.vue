ba88fbfdc1d442c8e75bf7175c15fd1505d7aeb5
Teuku Rifki Dhulul Fata <u073590@bca.co.id>
2023-10-25 17:37:05 +0700

    styling

--- src/pages/short.vue
+++ src/pages/short.vue
@@ -1,72 +1,47 @@
 <template>
-  <div class="q-pa-md example-row-all-breakpoints">
-    <!-- <div class="q-gutter-y-md column" style="max-width: 1000px"> -->
-    <div class="row items-center">
-      <div class="col-2 q-mr-xs" align="right">https:// </div>
-      <div class="col-10">
-        <q-input class="my-element" outlined v-model="sourcePath" for="txtSourcePath" placeholder="www.verylongendpoint.com" :dense="dense">
-          <template v-slot:after>
-            <q-icon name="close" @click="sourcePath = ''" class="cursor-pointer" />
-          </template>
-        </q-input>
-      </div>
+  <div class="q-pa-md center-screen">
+    <div class="q-subheading">Made With 🖕 by <a href="https://instagram.com/rifkifata">Teuku</a>
     </div>
-    <div class="row items-center">
-      <div class="col-2" align="right">teuku.my.id/</div>
-      <div class="col-10">
-        <q-input outlined v-model="shortedPath" for="txtShortedPath" placeholder="short" :dense="dense">
-          <template v-slot:after>
-            <q-icon name="close" @click="shortedPath = ''" class="cursor-pointer" />
-          </template>
-        </q-input>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="col-2" align="right">Submit By : </div>
-      <div class="col-10">
-        <q-input outlined v-model="author" for="txtAuthor" placeholder="ikyganteng" :dense="dense">
-          <template v-slot:after>
-            <q-icon name="close" @click="author = ''" class="cursor-pointer" />
-          </template>
-        </q-input>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="col-2" align="right">
-        <!--        <q-btn class="rotate-90" round color="black" icon="navigation" onclick="this.style['WebkitTransform'] = 'rotate(' + 180 +'deg)';validate()" /> -->
-        <q-btn class="rotate-90" round color="black" icon="navigation" @click="validate()" />
-
-      </div>
-      <!-- hint="Made with Pride by Teuku Rifki Dhulul Fata" -->
-      <div class="col-10">
-        <q-input outlined v-model="result" :dense="dense" readonly>
-          <template v-slot:after>
-            <q-icon id="txtResult" name="content_copy" @click="copyToClipBoard()" class="cursor-pointer" />
-          </template>
-          <template v-slot:hint>
-            Destination
-          </template>
-        </q-input>
-      </div>
-    </div>
-    <div class="row item-center">
-      <div class="col-2"></div>
-      <div class="col10"></div>
-    </div>
+    <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+      <q-input outlined v-model="sourcePath" for="txtSourcePath" placeholder="www.verylongendpoint.com/fuckyou" hint="Your Destination Path Here!" :dense="dense">
+        <template v-slot:after>
+          <q-icon name="close" @click="sourcePath = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+      <q-input outlined v-model="shortedPath" for="txtShortedPath" placeholder="fuckyou" hint="Will Be : teuku.my.id/fuckyou" :dense="dense">
+        <template v-slot:after>
+          <q-icon name="close" @click="shortedPath = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+      <q-input outlined v-model="author" for="txtAuthor"  label="By The Name Of" placeholder="Ikyganteng" hint="Not Required" :dense="dense">
+        <template v-slot:after>
+          <q-icon name="close" @click="author = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+      <q-btn class="arrow_downward" for="btnSend" round color="black" icon="arrow_downward" @click="posts()" />
+      <q-input outlined v-model="result" :dense="dense" readonly>
+        <template v-slot:after>
+          <q-icon for="txtResult" name="content_copy" @click="copyToClipBoard()" class="cursor-pointer" />
+          <!-- copied -->
+        </template>
+      </q-input>
+    </q-form>
   </div>
-  <!-- </div> -->
-
-
-
 </template>
 <style>
-  .example-row-all-breakpoints {
-    margin-top: 1rem;
+.center-screen {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
   }
-  
 </style>
 
 <script lang="ts">
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
   // Import the functions you need from the SDKs you need
   import {
     initializeApp
@@ -75,30 +50,46 @@
     getAnalytics
   } from "firebase/analytics";
   import 'animate.css';
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBRWZ5eeZ1YpgYTgVqEGpzAS6iTa3elDy0",
-    authDomain: "expenses-bae47.firebaseapp.com",
-    projectId: "expenses-bae47",
-    storageBucket: "expenses-bae47.appspot.com",
-    messagingSenderId: "585378571530",
-    appId: "1:585378571530:web:3b6a6d83688e3f2000c6f3",
-    measurementId: "G-PPRPC8F4ZG"
-  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  // carousel
-  import {
-    ref
-  } from 'vue'
+
   export default {
     name: 'PageIndex',
     setup() {
-      return {}
+      const $q = useQuasar()
+
+    const name = ref(null)
+    const age = ref(null)
+    const accept = ref(false)
+
+    return {
+      name,
+      age,
+      accept,
+
+      onSubmit () {
+        if (accept.value !== true) {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'You need to accept the license and terms first'
+          })
+        }
+        else {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+        }
+      },
+
+      onReset () {
+        name.value = null
+        age.value = null
+        accept.value = false
+      }
+    }
     },
     data() {
       return {
@@ -119,7 +110,9 @@
       async copyToClipBoard() {
         await navigator.clipboard.writeText(this.result);
       },
-      async post() {
+      async posts() {
+        var validate = this.validate()
+        if (validate == true){
         await this.$axios.post('https://teuku.cyclic.app/short/', {
           "sourcePath": this.sourcePath,
           "shortedPath": this.shortedPath,
@@ -133,20 +126,21 @@
             console.log(err.response.data)
             this.result = err.response.data.message
           })
+        }
+
       },
       validate() {
         var sourcePath = this.sourcePath
         var shortedPath = this.shortedPath
         var author = this.author
         var element = document.getElementById('txtSourcePath');
-        if (sourcePath == "") {
+        if (sourcePath == "" || shortedPath == "") {
           animateCSS('#txtSourcePath', 'shakeX');
-          console.log("sourcePath Cannot Empty")
-        }
-        if (shortedPath == "") {
           animateCSS('#txtShortedPath', 'shakeX');
-          console.log("shortedPath Cannot Empty")
+          console.log("sourcePath or shortedPath Cannot Empty")
+          return false
         }
+        animateCSS('.arrow_downward', 'shakeY');
         return true
       }
     }
